190f0db6524b7ed05f496e49c791c0eb094bbff6
Ashley <ashley@Jamess-MacBook-Air.local>
2018-09-28 14:59:52 -0500

    beginning deck object constructor

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//deck object
+//moves for card start left to right. 
+const deck = [{
+	name: "Cobra",
+	redmovement1x: -1
+	redmovement1y: +0
+	redmovement2x: +1
+	redmovement2y: -1
+	redmovement3x: +1
+	redmovement3y: +1
+	bluemovement1x: +1
+	bluemovement1y: +0
+	bluemovement2x: -1
+	bluemovement2y: +1
+	bluemovement3x: -1
+	bluemovement3y: -1
+	startingposition: null; //when card is selected, assign this to pawn's position. 
+
+}, {
+	name: "Cobra",
+	redmovement1x: -1
+	redmovement1y: +0
+	redmovement2x: +1
+	redmovement2y: -1
+	redmovement3x: +1
+	redmovement3y: +1
+	bluemovement1x: +1
+	bluemovement1y: +0
+	bluemovement2x: -1
+	bluemovement2y: +1
+	bluemovement3x: -1
+	bluemovement3y: -1
+	startingposition: null; //when card is selected, assign this to pawn's position. 
+
+}
+
+
+
+
+]
