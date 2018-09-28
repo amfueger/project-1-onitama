584a9268283e20cfb5e656ba245923e8440faff6
Ashley <ashley@Jamess-MacBook-Air.local>
2018-09-28 16:17:41 -0500

    worked on cards, moving on to creating board

--- js/app.js
+++ js/app.js
@@ -42,18 +42,18 @@ const deck = [{
 	redmovement1y: +1
 	redmovement2x: -1
 	redmovement2y: +0
-	redmovement3x: 
-	redmovement3y: 
-	redmovement4x:
-	redmovement4y:
-	bluemovement1x: 
-	bluemovement1y: 
-	bluemovement2x: 
-	bluemovement2y: 
-	bluemovement3x: 
-	bluemovement3y: 
-	bluemovement4x:
-	bluemovement4y:
+	redmovement3x: +1
+	redmovement3y: +1
+	redmovement4x: +1
+	redmovement4y: +0
+	bluemovement1x: +1 
+	bluemovement1y: -1
+	bluemovement2x: +1
+	bluemovement2y: +0
+	bluemovement3x: -1
+	bluemovement3y: -1
+	bluemovement4x: -1
+	bluemovement4y: +0
 	startingposition: null, //when card is selected, assign this to pawn's position. 
 
 }, {
@@ -78,6 +78,7 @@ const deck = [{
 //redPawn[0].x = redPawn[0].x + card[1].redmovement1x
 //redPawn[0].y = redPawn[0].y + card[1].redmovement1Y
 //make sure these stay integers
+//run check on pawn's x and y coordinates after, then if pawns x,y matches square's x,y, move it. 
 
 
 
