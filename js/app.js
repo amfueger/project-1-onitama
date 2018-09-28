7b60e4cd3c48ee48798c73fd64d10892d7cf0b0f
Ashley <ashley@Jamess-MacBook-Air.local>
2018-09-28 15:21:36 -0500

    added two cards

--- js/app.js
+++ js/app.js
@@ -2,35 +2,75 @@
 //moves for card start left to right. 
 const deck = [{
 	name: "Cobra",
-	redmovement1x: -1
-	redmovement1y: +0
-	redmovement2x: +1
-	redmovement2y: -1
-	redmovement3x: +1
-	redmovement3y: +1
-	bluemovement1x: +1
-	bluemovement1y: +0
-	bluemovement2x: -1
-	bluemovement2y: +1
-	bluemovement3x: -1
-	bluemovement3y: -1
-	startingposition: null; //when card is selected, assign this to pawn's position. 
+	redmovement1x: -1,
+	redmovement1y: +0,
+	redmovement2x: +1,
+	redmovement2y: -1,
+	redmovement3x: +1,
+	redmovement3y: +1,
+	bluemovement1x: +1,
+	bluemovement1y: +0,
+	bluemovement2x: -1,
+	bluemovement2y: +1,
+	bluemovement3x: -1,
+	bluemovement3y: -1,
+	startingposition: null, //when card is selected, assign this to pawn's position. 
 
 }, {
-	name: "Cobra",
-	redmovement1x: -1
-	redmovement1y: +0
-	redmovement2x: +1
-	redmovement2y: -1
-	redmovement3x: +1
-	redmovement3y: +1
-	bluemovement1x: +1
-	bluemovement1y: +0
-	bluemovement2x: -1
-	bluemovement2y: +1
-	bluemovement3x: -1
-	bluemovement3y: -1
-	startingposition: null; //when card is selected, assign this to pawn's position. 
+	name: "Goose",
+	redmovement1x: -1,
+	redmovement1y: +1,
+	redmovement2x: -1,
+	redmovement2y: +0,
+	redmovement3x: +1,
+	redmovement3y: +0,
+	redmovement4x: +1,
+	redmovement4y: -1,
+	bluemovement1x: +1,
+	bluemovement1y: -1,
+	bluemovement2x: +1,
+	bluemovement2y: +0,
+	bluemovement3x: -1,
+	bluemovement3y: +0,
+	bluemovement4x: -1,
+	bluemovement4y: +1,
+	startingposition: null, //when card is selected, assign this to pawn's position. 
+
+}, {
+	name: "Elephant",
+	redmovement1x: 
+	redmovement1y: 
+	redmovement2x: 
+	redmovement2y: 
+	redmovement3x: 
+	redmovement3y: 
+	redmovement4x:
+	redmovement4y:
+	bluemovement1x: 
+	bluemovement1y: 
+	bluemovement2x: 
+	bluemovement2y: 
+	bluemovement3x: 
+	bluemovement3y: 
+	bluemovement4x:
+	bluemovement4y:
+	startingposition: null, //when card is selected, assign this to pawn's position. 
+
+}, {
+	name: "Frog",
+	redmovement1x: 
+	redmovement1y: 
+	redmovement2x: 
+	redmovement2y: 
+	redmovement3x: 
+	redmovement3y: 
+	bluemovement1x: 
+	bluemovement1y: 
+	bluemovement2x: 
+	bluemovement2y: 
+	bluemovement3x: 
+	bluemovement3y: 
+	startingposition: null, //when card is selected, assign this to pawn's position. 
 
 }
 
