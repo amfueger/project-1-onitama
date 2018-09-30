28251db9083cece497f5b753e21796586381c760
Ashley <ashley@Jamess-MacBook-Air.local>
2018-09-29 20:25:26 -0500

    winnowed down the pawn class, realized that making six pawns in the class was not as good as using the pawn class to generate a single paw and dealing with any special properties outside.

--- js/app.js
+++ js/app.js
@@ -110,77 +110,14 @@ redPawn[0].y = redPawn[0].y + card[1].redmovement1Y
 */
 //Pawn class
 class Pawn{
-	constructor(x, y, color, pawnnumber, sensei){
+	constructor(x, y, color, pawnnumber){
 		this.x: pawnSetup(),
 		this.y: pawnSetup(), 
-		this.color: null;
-		this.pawnnumber: 
-		this.sensei: this.isSensei();
-
-	},
-	isSensei() {
-		if(this.pawnnumber == 3) {
-			this.sensei = true;
-		} 
-		return false;
-	},
-	senseiStone(){
-
-	},
-	senseiStream(){
-
-	},
-	pawnSetup(){
-		if(this.color == "red"){
-			for(let i = 0; i <= 5; i++) {
-				this.pawnnumber = i;
-				}
-				while(this.pawnnumber = 1) {
-					this.x = 1;
-					this.y = 1;
-				}
-				while(this.pawnnumber == 2){
-					this.x = 2;
-					this.y = 1;
-				}
-				while(this.pawnnumber == 3){
-					this.x = 3;
-					this.y = 1;
-				}
-				while(this.pawnnumber == 4){
-					this.x = 4;
-					this.y = 1;
-				}
-				while(this.pawnnumber == 5){
-					this.x = 5;
-					this.y = 1;
-				}
-			}
-		if(this.color == "blue"){
-			for(let i = 0; i <= 5; i++) {
-				this.pawnnumber = i;
-				}
-				while(this.pawnnumber = 1) {
-					this.x = 1;
-					this.y = 5;
-				}
-				while(this.pawnnumber == 2){
-					this.x = 2;
-					this.y = 5;
-				}
-				while(this.pawnnumber == 3){
-					this.x = 3;
-					this.y = 5;
-				}
-				while(this.pawnnumber == 4){
-					this.x = 4;
-					this.y = 5;
-				}
-				while(this.pawnnumber == 5){
-					this.x = 5;
-					this.y = 5;
-				}
-			}
+		this.color: null,
+		this.pawnnumber: 0,
+		this.live: true,
+	// don't need sensei because it's always three. 
+	//storing pawns in an array, will hide within the array using boolean instead of removing because otherwise that changes the position of the pawns upon calling them. 
 	},
 }
 
@@ -191,18 +128,80 @@ const game = {
 
 //pass in the coordinates for redmovementx and y on Blue's turn.
 //Make sure arguments comes in as a number!!!! 
-convertMovementForBlue(redmovementx, redmovementy) {
-	let blueMovementX = (blueMovementX * -1);
-	let blueMovementY = (blueMovementY * -1);
-	return blueMovementX, blueMovementY
-}
+//define the movement in the card for one of the pieces. When I write the moving piece, it checks to see if the pawn is blue, it multiplies the movement by -1
 
 
+	gameSetup() {
+		///generate 5 pawns for each color
+
+	}
 
 
+	senseiStone(pawnnumber){
 
+	},
+	senseiStream(x, y){
 
+	},
+	redPawnGeneration() {
+		pawnSetup() {
+			if (this.color == "red") {
+				for (let i = 0; i <= 5; i++) {
+					this.pawnnumber = i;
+				}
+				while (this.pawnnumber = 1) {
+					this.x = 1;
+					this.y = 1;
+				}
+				while (this.pawnnumber == 2) {
+					this.x = 2;
+					this.y = 1;
+				}
+				while (this.pawnnumber == 3) {
+					this.x = 3;
+					this.y = 1;
+				}
+				while (this.pawnnumber == 4) {
+					this.x = 4;
+					this.y = 1;
+				}
+				while (this.pawnnumber == 5) {
+					this.x = 5;
+					this.y = 1;
+				}
+			}
+		}
+	}, bluePawnGeneration() {
+		if (this.color == "blue") {
+				for (let i = 0; i <= 5; i++) {
+					this.pawnnumber = i;
+				}
+				while (this.pawnnumber = 1) {
+					this.x = 1;
+					this.y = 5;
+				}
+				while (this.pawnnumber == 2) {
+					this.x = 2;
+					this.y = 5;
+				}
+				while (this.pawnnumber == 3) {
+					this.x = 3;
+					this.y = 5;
+				}
+				while (this.pawnnumber == 4) {
+					this.x = 4;
+					this.y = 5;
+				}
+				while (this.pawnnumber == 5) {
+					this.x = 5;
+					this.y = 5;
+				}
+				return
+			}
+		}
+	},
 
+	//where do the pawns live? They are objects. Simplest way is to have the game class have an array where pawn objects live. Consider using a dead boolean value.
 
 
 
